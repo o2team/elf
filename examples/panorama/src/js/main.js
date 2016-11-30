4d94f4473455b840e4d38e140b136c7e845234a8
Liucw <liucw.mail@gmail.com>
2016-11-30 15:00:05 +0800

    update example panorama

--- examples/panorama/src/js/main.js
+++ examples/panorama/src/js/main.js
@@ -6,6 +6,7 @@ require('three/examples/js/renderers/Projector.js')
 require('three/examples/js/renderers/CanvasRenderer.js')
 
 var Preloader = require('preloader.js')
+var Orientation = require('orientation.js')
 
 var images = [
     'img/pano1-s.jpg',
@@ -19,18 +20,25 @@ require('../img/pano2-s.jpg')
 require('../img/pano3-s.jpg')
 require('../img/pano4-s.jpg')
 
+var orientation
 var camera, scene, renderer
 var texture_placeholder,
     isUserInteracting = false,
-    onMouseDownMouseX = 0,
-    onMouseDownMouseY = 0,
+    onPointerDownPointerX = 0,
+    onPointerDownPointerY = 0,
     lon = 90,
-    onMouseDownLon = 0,
     lat = 0,
-    onMouseDownLat = 0,
     phi = 0,
     theta = 0,
-    target = new THREE.Vector3()
+    target = new THREE.Vector3(),
+    lonOrientation = 0,
+    latOrientation = 0,
+    lonOffset = 0,
+    latOffset = 0,
+    startTime = 0,
+    startLon = 0,
+    startLat = 0,
+    duraTime = 0
 
 /**
  * init
@@ -39,7 +47,7 @@ function init() {
     console.log('init ok')
     var container, mesh
     container = document.getElementById('container')
-    camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 1, 1100)
+    camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 1, 1100)
 
     scene = new THREE.Scene()
     texture_placeholder = document.createElement('canvas')
@@ -73,10 +81,10 @@ function init() {
     document.addEventListener('mousedown', onDocumentMouseDown, false)
     document.addEventListener('mousemove', onDocumentMouseMove, false)
     document.addEventListener('mouseup', onDocumentMouseUp, false)
-    document.addEventListener('wheel', onDocumentMouseWheel, false)
     document.addEventListener('touchstart', onDocumentTouchStart, false)
     document.addEventListener('touchmove', onDocumentTouchMove, false)
-    //
+    document.addEventListener('touchend', onDocumentTouchEnd, false)
+    document.addEventListener('touchcancel', onDocumentTouchEnd, false)
     window.addEventListener('resize', onWindowResize, false)
 }
 
@@ -115,7 +123,6 @@ function loadTexture(path) {
 
 function onDocumentMouseDown(event) {
     event.preventDefault()
-    isUserInteracting = true
     onPointerDownPointerX = event.clientX
     onPointerDownPointerY = event.clientY
     onPointerDownLon = lon
@@ -123,19 +130,11 @@ function onDocumentMouseDown(event) {
 }
 
 function onDocumentMouseMove(event) {
-    if (isUserInteracting === true) {
-        lon = (onPointerDownPointerX - event.clientX) * 0.1 + onPointerDownLon
-        lat = (event.clientY - onPointerDownPointerY) * 0.1 + onPointerDownLat
-    }
+    lon = (onPointerDownPointerX - event.clientX) * 0.1 + onPointerDownLon
+    lat = (event.clientY - onPointerDownPointerY) * 0.1 + onPointerDownLat
 }
 
 function onDocumentMouseUp(event) {
-    isUserInteracting = false
-}
-
-function onDocumentMouseWheel(event) {
-    camera.fov += event.deltaY * 0.05
-    camera.updateProjectionMatrix()
 }
 
 function onDocumentTouchStart(event) {
@@ -145,6 +144,12 @@ function onDocumentTouchStart(event) {
         onPointerDownPointerY = event.touches[0].pageY
         onPointerDownLon = lon
         onPointerDownLat = lat
+
+        startTime = Date.now()
+        startLon = event.touches[0].pageX
+        startLat = event.touches[0].pageY
+        lonOffset = 0
+        latOffset = 0
     }
 }
 
@@ -152,8 +157,17 @@ function onDocumentTouchMove(event) {
     if (event.touches.length == 1) {
         event.preventDefault()
         lon = (onPointerDownPointerX - event.touches[0].pageX) * 0.2 + onPointerDownLon
-        lat = (event.touches[0].pageY - onPointerDownPointerY) * 0.2 + onPointerDownLat
+        lat = (event.touches[0].pageY - onPointerDownPointerY) * 0.1 + onPointerDownLat
     }
+}
+
+function onDocumentTouchEnd(event) {
+    duraTime = Date.now() - startTime
+    lonOffset = startLon - event.pageX
+    latOffset = event.pageY - startLat
+
+    lonOffset = (lonOffset/duraTime) * 50
+    latOffset = (latOffset/duraTime) * 50
 }
 
 function animate() {
@@ -162,10 +176,25 @@ function animate() {
 }
 
 function update() {
-    if (isUserInteracting === false) {
-        lon += 0.1
+    if (Math.abs(lonOrientation) > 0.01) {
+        lon += -lonOrientation
+        lonOrientation = 0
     }
-    lat = Math.max(-85, Math.min(85, lat))
+    if (Math.abs(latOrientation) > 0.01) {
+        lat += latOrientation
+        latOrientation = 0
+    }
+
+    if (Math.abs(lonOffset) > 0.01) {
+        lon += lonOffset * 0.05
+        lonOffset = lonOffset * 0.95
+    }
+    if (Math.abs(latOffset) > 0.01) {
+        lat += latOffset * 0.05
+        latOffset = latOffset * 0.95
+    }
+
+    lat = Math.max(-45, Math.min(45, lat))
     phi = THREE.Math.degToRad(90 - lat)
     theta = THREE.Math.degToRad(lon)
     target.x = 500 * Math.sin(phi) * Math.cos(theta)
@@ -173,6 +202,16 @@ function update() {
     target.z = 500 * Math.sin(phi) * Math.sin(theta)
     camera.lookAt(target)
     renderer.render(scene, camera)
+}
+
+function initOrientation () {
+    orientation = new Orientation({
+        onChange: function (e) {
+            lonOrientation += e.offsetLon
+            latOrientation += e.offsetLat
+        }
+    })
+    orientation.init()
 }
 
 /**
@@ -191,5 +230,6 @@ preloader.addCompletionListener(function () {
 
     init()
     animate()
+    initOrientation()
 })
 preloader.start()

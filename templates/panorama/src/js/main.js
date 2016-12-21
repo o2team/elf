2a2938a4d424633e0980b7df419e1ef8b2d32e59
Liucw <liucw.mail@gmail.com>
2016-12-21 15:59:28 +0800

    chore(template): 统一样式文件和js文件的缩进

--- templates/panorama/src/js/main.js
+++ templates/panorama/src/js/main.js
@@ -9,10 +9,10 @@ var Preloader = require('preloader.js')
 var Orientation = require('orientation.js')
 
 var images = [
-    'img/pano1-s.jpg',
-    'img/pano2-s.jpg',
-    'img/pano3-s.jpg',
-    'img/pano4-s.jpg',
+  'img/pano1-s.jpg',
+  'img/pano2-s.jpg',
+  'img/pano3-s.jpg',
+  'img/pano4-s.jpg',
 ]
 
 require('../img/pano1-s.jpg')
@@ -23,213 +23,215 @@ require('../img/pano4-s.jpg')
 var orientation
 var camera, scene, renderer
 var texture_placeholder,
-    isUserInteracting = false,
-    onPointerDownPointerX = 0,
-    onPointerDownPointerY = 0,
-    lon = 90,
-    lat = 0,
-    phi = 0,
-    theta = 0,
-    target = new THREE.Vector3(),
-    lonOrientation = 0,
-    latOrientation = 0,
-    lonOffset = 0,
-    latOffset = 0,
-    startTime = 0,
-    startLon = 0,
-    startLat = 0,
-    duraTime = 0
+  isUserInteracting = false,
+  onPointerDownPointerX = 0,
+  onPointerDownPointerY = 0,
+  lon = 90,
+  lat = 0,
+  phi = 0,
+  theta = 0,
+  target = new THREE.Vector3(),
+  lonOrientation = 0,
+  latOrientation = 0,
+  lonOffset = 0,
+  latOffset = 0,
+  startTime = 0,
+  startLon = 0,
+  startLat = 0,
+  duraTime = 0
 
 /**
  * init
  */
 function init() {
-    console.log('init ok')
-    var container, mesh
-    container = document.getElementById('container')
-    camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 1, 1100)
+  console.log('init ok')
+  var container, mesh
+  container = document.getElementById('container')
+  camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 1, 1100)
 
-    scene = new THREE.Scene()
-    texture_placeholder = document.createElement('canvas')
-    texture_placeholder.width = 128
-    texture_placeholder.height = 128
-    var context = texture_placeholder.getContext('2d')
-    context.fillStyle = 'rgb( 200, 200, 200 )'
-    context.fillRect(0, 0, texture_placeholder.width, texture_placeholder.height)
-    var materials = [
-        loadTexture('img/pano3-s.jpg'), // right
-        loadTexture('img/pano2-s.jpg'), // left
-        new THREE.MeshBasicMaterial( {color: 0x90C0C0} ), // top
-        new THREE.MeshBasicMaterial( {color: 0x9F5D3B} ), // bottom
-        loadTexture('img/pano4-s.jpg'), // back
-        loadTexture('img/pano1-s.jpg') // front
-    ]
-    mesh = new THREE.Mesh(new THREE.BoxGeometry(300, 300, 300, 7, 7, 7), new THREE.MultiMaterial(materials))
-    mesh.scale.x = -1
-    scene.add(mesh)
+  scene = new THREE.Scene()
+  texture_placeholder = document.createElement('canvas')
+  texture_placeholder.width = 128
+  texture_placeholder.height = 128
+  var context = texture_placeholder.getContext('2d')
+  context.fillStyle = 'rgb( 200, 200, 200 )'
+  context.fillRect(0, 0, texture_placeholder.width, texture_placeholder.height)
+  var materials = [
+    loadTexture('img/pano3-s.jpg'), // right
+    loadTexture('img/pano2-s.jpg'), // left
+    new THREE.MeshBasicMaterial({
+      color: 0x90C0C0
+    }), // top
+    new THREE.MeshBasicMaterial({
+      color: 0x9F5D3B
+    }), // bottom
+    loadTexture('img/pano4-s.jpg'), // back
+    loadTexture('img/pano1-s.jpg') // front
+  ]
+  mesh = new THREE.Mesh(new THREE.BoxGeometry(300, 300, 300, 7, 7, 7), new THREE.MultiMaterial(materials))
+  mesh.scale.x = -1
+  scene.add(mesh)
 
-	if ( webglAvailable() ) {
-		renderer = new THREE.WebGLRenderer();
-	} else {
-		renderer = new THREE.CanvasRenderer();
-	}
+  if (webglAvailable()) {
+    renderer = new THREE.WebGLRenderer();
+  } else {
+    renderer = new THREE.CanvasRenderer();
+  }
 
-    renderer.setPixelRatio(window.devicePixelRatio)
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    container.appendChild(renderer.domElement)
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  container.appendChild(renderer.domElement)
 
-    document.addEventListener('mousedown', onDocumentMouseDown, false)
-    document.addEventListener('mousemove', onDocumentMouseMove, false)
-    document.addEventListener('mouseup', onDocumentMouseUp, false)
-    document.addEventListener('touchstart', onDocumentTouchStart, false)
-    document.addEventListener('touchmove', onDocumentTouchMove, false)
-    document.addEventListener('touchend', onDocumentTouchEnd, false)
-    document.addEventListener('touchcancel', onDocumentTouchEnd, false)
-    window.addEventListener('resize', onWindowResize, false)
+  document.addEventListener('mousedown', onDocumentMouseDown, false)
+  document.addEventListener('mousemove', onDocumentMouseMove, false)
+  document.addEventListener('mouseup', onDocumentMouseUp, false)
+  document.addEventListener('touchstart', onDocumentTouchStart, false)
+  document.addEventListener('touchmove', onDocumentTouchMove, false)
+  document.addEventListener('touchend', onDocumentTouchEnd, false)
+  document.addEventListener('touchcancel', onDocumentTouchEnd, false)
+  window.addEventListener('resize', onWindowResize, false)
 }
 
 function webglAvailable() {
-    try {
-        var canvas = document.createElement( 'canvas' );
-        return !!( window.WebGLRenderingContext && (
-            canvas.getContext( 'webgl' ) ||
-            canvas.getContext( 'experimental-webgl' ) )
-        );
-    } catch ( e ) {
-        return false;
-    }
+  try {
+    var canvas = document.createElement('canvas');
+    return !!(window.WebGLRenderingContext && (
+      canvas.getContext('webgl') ||
+      canvas.getContext('experimental-webgl')));
+  } catch (e) {
+    return false;
+  }
 }
 
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
 }
 
 function loadTexture(path) {
-    var texture = new THREE.Texture(texture_placeholder)
-    var material = new THREE.MeshBasicMaterial({
-        map: texture,
-        overdraw: 0.5
-    })
-    var image = new Image()
-    image.onload = function () {
-        texture.image = this
-        texture.needsUpdate = true
-    }
-    image.src = path
-    return material
+  var texture = new THREE.Texture(texture_placeholder)
+  var material = new THREE.MeshBasicMaterial({
+    map: texture,
+    overdraw: 0.5
+  })
+  var image = new Image()
+  image.onload = function () {
+    texture.image = this
+    texture.needsUpdate = true
+  }
+  image.src = path
+  return material
 }
 
 function onDocumentMouseDown(event) {
-    event.preventDefault()
-    onPointerDownPointerX = event.clientX
-    onPointerDownPointerY = event.clientY
-    onPointerDownLon = lon
-    onPointerDownLat = lat
+  event.preventDefault()
+  onPointerDownPointerX = event.clientX
+  onPointerDownPointerY = event.clientY
+  onPointerDownLon = lon
+  onPointerDownLat = lat
 }
 
 function onDocumentMouseMove(event) {
-    lon = (onPointerDownPointerX - event.clientX) * 0.1 + onPointerDownLon
-    lat = (event.clientY - onPointerDownPointerY) * 0.1 + onPointerDownLat
+  lon = (onPointerDownPointerX - event.clientX) * 0.1 + onPointerDownLon
+  lat = (event.clientY - onPointerDownPointerY) * 0.1 + onPointerDownLat
 }
 
-function onDocumentMouseUp(event) {
-}
+function onDocumentMouseUp(event) {}
 
 function onDocumentTouchStart(event) {
-    if (event.touches.length == 1) {
-        event.preventDefault()
-        onPointerDownPointerX = event.touches[0].pageX
-        onPointerDownPointerY = event.touches[0].pageY
-        onPointerDownLon = lon
-        onPointerDownLat = lat
+  if (event.touches.length == 1) {
+    event.preventDefault()
+    onPointerDownPointerX = event.touches[0].pageX
+    onPointerDownPointerY = event.touches[0].pageY
+    onPointerDownLon = lon
+    onPointerDownLat = lat
 
-        startTime = Date.now()
-        startLon = event.touches[0].pageX
-        startLat = event.touches[0].pageY
-        lonOffset = 0
-        latOffset = 0
-    }
+    startTime = Date.now()
+    startLon = event.touches[0].pageX
+    startLat = event.touches[0].pageY
+    lonOffset = 0
+    latOffset = 0
+  }
 }
 
 function onDocumentTouchMove(event) {
-    if (event.touches.length == 1) {
-        event.preventDefault()
-        lon = (onPointerDownPointerX - event.touches[0].pageX) * 0.2 + onPointerDownLon
-        lat = (event.touches[0].pageY - onPointerDownPointerY) * 0.1 + onPointerDownLat
-    }
+  if (event.touches.length == 1) {
+    event.preventDefault()
+    lon = (onPointerDownPointerX - event.touches[0].pageX) * 0.2 + onPointerDownLon
+    lat = (event.touches[0].pageY - onPointerDownPointerY) * 0.1 + onPointerDownLat
+  }
 }
 
 function onDocumentTouchEnd(event) {
-    duraTime = Date.now() - startTime
-    lonOffset = startLon - event.pageX
-    latOffset = event.pageY - startLat
+  duraTime = Date.now() - startTime
+  lonOffset = startLon - event.pageX
+  latOffset = event.pageY - startLat
 
-    lonOffset = (lonOffset/duraTime) * 50
-    latOffset = (latOffset/duraTime) * 50
+  lonOffset = (lonOffset / duraTime) * 50
+  latOffset = (latOffset / duraTime) * 50
 }
 
 function animate() {
-    requestAnimationFrame(animate)
-    update()
+  requestAnimationFrame(animate)
+  update()
 }
 
 function update() {
-    if (Math.abs(lonOrientation) > 0.01) {
-        lon += -lonOrientation
-        lonOrientation = 0
-    }
-    if (Math.abs(latOrientation) > 0.01) {
-        lat += latOrientation
-        latOrientation = 0
-    }
+  if (Math.abs(lonOrientation) > 0.01) {
+    lon += -lonOrientation
+    lonOrientation = 0
+  }
+  if (Math.abs(latOrientation) > 0.01) {
+    lat += latOrientation
+    latOrientation = 0
+  }
 
-    if (Math.abs(lonOffset) > 0.01) {
-        lon += lonOffset * 0.05
-        lonOffset = lonOffset * 0.95
-    }
-    if (Math.abs(latOffset) > 0.01) {
-        lat += latOffset * 0.05
-        latOffset = latOffset * 0.95
-    }
+  if (Math.abs(lonOffset) > 0.01) {
+    lon += lonOffset * 0.05
+    lonOffset = lonOffset * 0.95
+  }
+  if (Math.abs(latOffset) > 0.01) {
+    lat += latOffset * 0.05
+    latOffset = latOffset * 0.95
+  }
 
-    lat = Math.max(-45, Math.min(45, lat))
-    phi = THREE.Math.degToRad(90 - lat)
-    theta = THREE.Math.degToRad(lon)
-    target.x = 500 * Math.sin(phi) * Math.cos(theta)
-    target.y = 700 * Math.cos(phi)
-    target.z = 500 * Math.sin(phi) * Math.sin(theta)
-    camera.lookAt(target)
-    renderer.render(scene, camera)
+  lat = Math.max(-45, Math.min(45, lat))
+  phi = THREE.Math.degToRad(90 - lat)
+  theta = THREE.Math.degToRad(lon)
+  target.x = 500 * Math.sin(phi) * Math.cos(theta)
+  target.y = 700 * Math.cos(phi)
+  target.z = 500 * Math.sin(phi) * Math.sin(theta)
+  camera.lookAt(target)
+  renderer.render(scene, camera)
 }
 
-function initOrientation () {
-    orientation = new Orientation({
-        onChange: function (e) {
-            lonOrientation += e.offsetLon
-            latOrientation += e.offsetLat
-        }
-    })
-    orientation.init()
+function initOrientation() {
+  orientation = new Orientation({
+    onChange: function (e) {
+      lonOrientation += e.offsetLon
+      latOrientation += e.offsetLat
+    }
+  })
+  orientation.init()
 }
 
 /**
  * preloader && start
  */
 var preloader = new Preloader({
-    resources: images,
-    perMinTime: 1000 // 加载每个资源所需的最小时间，一般用来测试 loading
+  resources: images,
+  perMinTime: 1000 // 加载每个资源所需的最小时间，一般用来测试 loading
 })
 preloader.addProgressListener(function (loaded, length) {
-    console.log('loaded', loaded, length, loaded / length)
+  console.log('loaded', loaded, length, loaded / length)
 })
 preloader.addCompletionListener(function () {
-    $('#o2_loading').remove()
-    $('#o2_main').removeClass('hide')
+  $('#o2_loading').remove()
+  $('#o2_main').removeClass('hide')
 
-    init()
-    animate()
-    initOrientation()
+  init()
+  animate()
+  initOrientation()
 })
 preloader.start()
--- templates/switcher/src/js/main.js
+++ templates/switcher/src/js/main.js
@@ -13,64 +13,64 @@ var home = {}
 home.$ = $('.home')
 home.$txt = $('.home .home-txt')
 home.enter = function () {
-    anime({
+  anime({
+    targets: '.home-4',
+    scale: {
+      value: [3.8, 0.5],
+      delay: 0,
+      duration: 1000,
+      easing: 'easeInOutExpo',
+      elasticity: 800
+    },
+    complete: function () {
+      anime({
         targets: '.home-4',
         scale: {
-            value: [3.8, 0.5],
-            delay: 0,
-            duration: 1000,
-            easing: 'easeInOutExpo',
-            elasticity: 800
+          value: [0.5, 1.2],
+          duration: 1000,
+          easing: 'easeOutElastic',
+          elasticity: 600,
         },
-        complete: function () {
-            anime({
-                targets: '.home-4',
-                scale: {
-                    value: [0.5, 1.2],
-                    duration: 1000,
-                    easing: 'easeOutElastic',
-                    elasticity: 600,
-                },
-            })
-            anime({
-                targets: '.home-txt',
-                scale: {
-                    value: [0, 1],
-                    duration: 500,
-                    easing: 'easeInOutExpo'
-                }
-            })
-        }
-    })
-}
-home.leave = function (done) {
-    anime({
-        targets: '.home-4',
-        scale: {
-            value: [1, 3.8],
-            duration: 1000,
-            easing: 'easeInOutExpo'
-        },
-        complete: function () {
-            done && done()
-        }
-    })
-    anime({
+      })
+      anime({
         targets: '.home-txt',
         scale: {
-            value: [1, 0],
-            duration: 800,
-            easing: 'easeInOutExpo'
+          value: [0, 1],
+          duration: 500,
+          easing: 'easeInOutExpo'
         }
-    })
+      })
+    }
+  })
+}
+home.leave = function (done) {
+  anime({
+    targets: '.home-4',
+    scale: {
+      value: [1, 3.8],
+      duration: 1000,
+      easing: 'easeInOutExpo'
+    },
+    complete: function () {
+      done && done()
+    }
+  })
+  anime({
+    targets: '.home-txt',
+    scale: {
+      value: [1, 0],
+      duration: 800,
+      easing: 'easeInOutExpo'
+    }
+  })
 }
 home.stage = function () {
-    $('.home .home-4').css({
-        'transform': 'scale(1.2)'
-    })
-    $('.home .home-txt').css({
-        'transform': 'scale(1)'
-    })
+  $('.home .home-4').css({
+    'transform': 'scale(1.2)'
+  })
+  $('.home .home-txt').css({
+    'transform': 'scale(1)'
+  })
 }
 
 /**
@@ -80,80 +80,80 @@ var meet1 = {}
 meet1.$pic = $('.meet1 .meet-pic')
 
 meet1.prepare = function () {
-    $('.meet1 .meet-bubble').css({
-        'transform': 'scale(3)'
-    })
-    $('.meet1 .meet-txt-1, .meet1 .meet-coffee').css({
-        'transform': 'scale(0)'
-    })
-    meet1.$pic.removeClass('air-active')
+  $('.meet1 .meet-bubble').css({
+    'transform': 'scale(3)'
+  })
+  $('.meet1 .meet-txt-1, .meet1 .meet-coffee').css({
+    'transform': 'scale(0)'
+  })
+  meet1.$pic.removeClass('air-active')
 }
 meet1.enter = function (done) {
-    anime({
+  anime({
+    targets: '.meet1 .meet-bubble',
+    scale: {
+      value: [3, 0.5],
+      delay: 0,
+      duration: 500,
+      easing: 'easeInOutExpo',
+    },
+    complete: function () {
+      anime({
         targets: '.meet1 .meet-bubble',
         scale: {
-            value: [3, 0.5],
-            delay: 0,
-            duration: 500,
-            easing: 'easeInOutExpo',
+          value: [0.5, 1],
+          duration: 1000,
+          easing: 'easeOutElastic',
+          elasticity: 600
         },
-        complete: function () {
-            anime({
-                targets: '.meet1 .meet-bubble',
-                scale: {
-                    value: [0.5, 1],
-                    duration: 1000,
-                    easing: 'easeOutElastic',
-                    elasticity: 600
-                },
-            })
-            anime({
-                targets: ['.meet1 .meet-txt-1', '.meet1 .meet-coffee'],
-                scale: {
-                    value: [0, 1],
-                    duration: 500,
-                    easing: 'easeInOutExpo'
-                },
-                complete: function () {
-                    meet1.$pic.addClass('air-active')
-                    done && done()
-                }
-            })
-        }
-    })
-}
-meet1.leave = function (done) {
-    anime({
-        targets: ['.meet1 .meet-bubble'],
-        scale: {
-            value: [1, 0],
-            duration: 1000,
-            easing: 'easeInOutExpo'
-        }
-    })
-
-    anime({
+      })
+      anime({
         targets: ['.meet1 .meet-txt-1', '.meet1 .meet-coffee'],
         scale: {
-            value: [1, 0],
-            duration: 1000,
-            easing: 'easeInOutExpo'
-        },
-        opacity: {
-            value: [1, 0],
-            duration: 1000,
-            easing: 'easeInOutExpo'
+          value: [0, 1],
+          duration: 500,
+          easing: 'easeInOutExpo'
         },
         complete: function () {
-            done && done()
+          meet1.$pic.addClass('air-active')
+          done && done()
         }
-    })
+      })
+    }
+  })
+}
+meet1.leave = function (done) {
+  anime({
+    targets: ['.meet1 .meet-bubble'],
+    scale: {
+      value: [1, 0],
+      duration: 1000,
+      easing: 'easeInOutExpo'
+    }
+  })
+
+  anime({
+    targets: ['.meet1 .meet-txt-1', '.meet1 .meet-coffee'],
+    scale: {
+      value: [1, 0],
+      duration: 1000,
+      easing: 'easeInOutExpo'
+    },
+    opacity: {
+      value: [1, 0],
+      duration: 1000,
+      easing: 'easeInOutExpo'
+    },
+    complete: function () {
+      done && done()
+    }
+  })
 }
 meet1.stage = function () {
-    $('.meet1 .meet-bubble, .meet1 .meet-txt-1, .meet1 .meet-coffee').css({
-        'transform': 'scale(1)',
-        'opacity': '1'
-    })
+  $('.meet1 .meet-bubble, .meet1 .meet-txt-1, .meet1 .meet-coffee').css({
+    'transform': 'scale(1)',
+    'opacity': '1'
+  })
 }
 
 /**
@@ -162,100 +162,100 @@ meet1.stage = function () {
 var meet2 = {}
 meet2.$pic = $('.meet2 .meet-pic')
 meet2.prepare = function () {
-    $('.meet2 .meet-bubble').css({
-        'transform': 'scale(0) translateZ(0)'
-    })
-    $('.meet2 .meet-txt-2, .meet2 .meet-coffee').css({
-        'transform': 'scale(0)'
-    })
-    meet2.$pic.removeClass('air-active')
+  $('.meet2 .meet-bubble').css({
+    'transform': 'scale(0) translateZ(0)'
+  })
+  $('.meet2 .meet-txt-2, .meet2 .meet-coffee').css({
+    'transform': 'scale(0)'
+  })
+  meet2.$pic.removeClass('air-active')
 }
 meet2.enter = function (done) {
-    anime({
-        targets: '.meet2 .meet-bubble',
-        scale: {
-            value: [0, 1],
-            duration: 1200,
-            easing: 'easeOutElastic',
-            elasticity: 600
-        },
-        translateZ: 0
-    })
+  anime({
+    targets: '.meet2 .meet-bubble',
+    scale: {
+      value: [0, 1],
+      duration: 1200,
+      easing: 'easeOutElastic',
+      elasticity: 600
+    },
+    translateZ: 0
+  })
 
-    anime({
-        targets: ['.meet2 .meet-txt-2', '.meet2 .meet-coffee'],
-        scale: {
-            value: [0, 1],
-            duration: 600,
-            easing: 'easeInOutExpo'
-        },
-        complete: function () {
-            meet2.$pic.addClass('air-active')
-            done && done()
-        }
-    })
+  anime({
+    targets: ['.meet2 .meet-txt-2', '.meet2 .meet-coffee'],
+    scale: {
+      value: [0, 1],
+      duration: 600,
+      easing: 'easeInOutExpo'
+    },
+    complete: function () {
+      meet2.$pic.addClass('air-active')
+      done && done()
+    }
+  })
 }
 
 
 function init(done) {
-    var isSwipering = false;
+  var isSwipering = false;
 
-    var mySwiper = new Swiper('#o2_swiper', {
-        pagination: '.swiper-pagination',
-        paginationClickable: true,
-        direction: 'vertical',
-        followFinger: false,
-        speed: 0,
-        onTouchEnd: function (swiper) {
-            if (isSwipering) return
-            isSwipering = true
-            mySwiper.lockSwipes()
-            if (swiper.swipeDirection === 'prev') {
-                mySwiper.unlockSwipes()
-                mySwiper.slidePrev(true, 300)
-                isSwipering = false
-            } else if (swiper.swipeDirection === 'next') {
-                if (swiper.activeIndex === 0) {
-                    home.leave(function () {
-                        mySwiper.unlockSwipes()
-                        mySwiper.slideNext(true, 0)
-                        meet1.enter()
-                        home.stage()
-                        meet2.prepare()
-                    })
-                } else if (swiper.activeIndex === 1) {
-                    meet1.leave(function () {
-                        mySwiper.unlockSwipes()
-                        mySwiper.slideNext(true, 0)
-                        meet2.enter()
-                        meet1.stage()
-                    })
-                } else {
-                    mySwiper.unlockSwipes()
-                    mySwiper.slideNext(true, 300)
-                    isSwipering = false
-                }
-            } else {
-                isSwipering = false
-            }
-
-        },
-        onTransitionEnd: function (swiper) {
-            var activeIndex = swiper.activeIndex
-
-            if (swiper.swipeDirection === 'prev') {
-                if (activeIndex === 0) {
-                    meet1.prepare()
-                } else if (activeIndex === 1) {
-                    meet2.prepare()
-                }
-            }
-
-            isSwipering = false
+  var mySwiper = new Swiper('#o2_swiper', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    direction: 'vertical',
+    followFinger: false,
+    speed: 0,
+    onTouchEnd: function (swiper) {
+      if (isSwipering) return
+      isSwipering = true
+      mySwiper.lockSwipes()
+      if (swiper.swipeDirection === 'prev') {
+        mySwiper.unlockSwipes()
+        mySwiper.slidePrev(true, 300)
+        isSwipering = false
+      } else if (swiper.swipeDirection === 'next') {
+        if (swiper.activeIndex === 0) {
+          home.leave(function () {
+            mySwiper.unlockSwipes()
+            mySwiper.slideNext(true, 0)
+            meet1.enter()
+            home.stage()
+            meet2.prepare()
+          })
+        } else if (swiper.activeIndex === 1) {
+          meet1.leave(function () {
+            mySwiper.unlockSwipes()
+            mySwiper.slideNext(true, 0)
+            meet2.enter()
+            meet1.stage()
+          })
+        } else {
+          mySwiper.unlockSwipes()
+          mySwiper.slideNext(true, 300)
+          isSwipering = false
         }
-    })
+      } else {
+        isSwipering = false
+      }
 
-    done && done()
+    },
+    onTransitionEnd: function (swiper) {
+      var activeIndex = swiper.activeIndex
+
+      if (swiper.swipeDirection === 'prev') {
+        if (activeIndex === 0) {
+          meet1.prepare()
+        } else if (activeIndex === 1) {
+          meet2.prepare()
+        }
+      }
+
+      isSwipering = false
+    }
+  })
+
+  done && done()
 }
 
 /**
@@ -263,12 +263,12 @@ function init(done) {
  */
 var preloader = new Preloader()
 preloader.addCompletionListener(function () {
-    $('#o2_main').removeClass('hide')
-    init()
-    setTimeout(function () {
-        $('#o2_loading').hide()
-        home.enter()
-        meet1.prepare()
-    }, 1000)
+  $('#o2_main').removeClass('hide')
+  init()
+  setTimeout(function () {
+    $('#o2_loading').hide()
+    home.enter()
+    meet1.prepare()
+  }, 1000)
 })
 preloader.start()

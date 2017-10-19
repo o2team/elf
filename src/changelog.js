e8c029501f22bbce49db0111cb930a2cee0d40e4
Liucw <liucw.mail@gmail.com>
2017-10-19 18:53:33 +0800

    chore: remove conventional-changelog

--- src/changelog.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-const conventionalChangelog = require('conventional-changelog')
-
-conventionalChangelog({
-  preset: 'angular'
-}).pipe(fs.createWriteStream(path.resolve(__dirname, '../doc/CHANGELOG.md')))

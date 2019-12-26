5986f3ae6d9697f28a526bde2554c209ee1ee8cb
Liucw <liucw.mail@gmail.com>
2019-12-26 15:41:14 +0800

    fix: 删除插件 npm-install-webpack-plugin

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -2,7 +2,7 @@ const path = require('path')
 const _ = require('lodash')
 const merge = require('webpack-merge')
 const webpack = require('webpack')
-const NpmInstallPlugin = require('npm-install-webpack-plugin')
+// const NpmInstallPlugin = require('npm-install-webpack-plugin')
 
 const allConfig = require('../config/index.js')
 const baseWebpackConfig = require('./webpack.base.js')
@@ -85,11 +85,11 @@ module.exports = merge(baseWebpackConfig, {
     }]
   },
   plugins: [
-    new NpmInstallPlugin({
-      dev: false,
-      peerDependencies: true,
-      quiet: false
-    }),
+    // new NpmInstallPlugin({
+    //   dev: false,
+    //   peerDependencies: true,
+    //   quiet: false
+    // }),
     new webpack.HotModuleReplacementPlugin()
   ],
   devServer: {
